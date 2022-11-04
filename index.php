bfdf100ab948420d8394d99cdf0adb500d7d1d16
black-code1 <legrandmahamat1@gmail.com>
2022-11-04 11:07:27 +0100

    Separate PHP Logic From the Template

--- index.php
+++ index.php
@@ -1,49 +1,30 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-</head>
-<body>
-    <h1>Recomended Books</h1>
-    <?php
-      $books = [
-              [
-              'name' => 'Do Androids Dreams of Electric Sheep',
-              'author' => 'Philip K. Dick',
-                  'releaseYear' => '1968',
-              'purchaseUrl' => 'http://example.com'
-              ],
-          [
-              'name' => 'Project Hail Mary',
-              'author' => 'Andy Weir',
-              'releaseYear' => '2021',
-              'purchaseUrl' => 'http://example.com'
-          ],
-          [
-              'name' => 'The Martian',
-              'author' => 'Andy Weir',
-              'releaseYear' => '2011',
-              'purchaseUrl' => 'http://example.com'
-          ]
-      ];
+<?php
+
+    $books = [
+        [
+            'name' => 'Do Androids Dreams of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'releaseYear' => '1968',
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'releaseYear' => '2021',
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => '2011',
+            'purchaseUrl' => 'http://example.com'
+        ]
+    ];
 
 
 
-      $filteredBooks = array_filter($books, function ($book) {
-          return $book['author'] === 'Andy Weir';
-      })
-    ?>
+    $filteredBooks = array_filter($books, function ($book) {
+        return $book['author'] === 'Andy Weir';
+    });
 
-    <ul>
-        <?php foreach ( $filteredBooks as $book) : ?>
-                <li>
-                    <a href="<?= $book['purchaseUrl'] ?>">
-                        <?= $book['name']; ?> (<?= $book['releaseYear']?>) - By <?= $book['author'] ?>
-                    </a>
-                </li>
-        <?php endforeach; ?>
-    </ul>
-
-</body>
-</html>
+    require "index.view.php";
